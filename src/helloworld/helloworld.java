3498cc7a2b9c7b45f0499b9924a401225c03009a
Erika <Erika@users.noreply.github.com>
2019-09-03 15:09:30 +0200

    commit3

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -7,6 +7,8 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input= new Scanner(System.in);
+		System.out.println("Användarnamn?");
+		String namn=input.nextLine();
 	}
 
 }
